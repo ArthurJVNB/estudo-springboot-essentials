5d5207cdf4e73e7b1c125d4717b8654c6862dedd
Arthur Vila Nova <arthurjvnb@yahoo.com.br>
2019-12-30 15:20:48 -0300

    implementacoes ate aula 09 (usando BD em MySQL)

--- src/main/java/br/com/devdojo/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdojo/endpoint/StudentEndpoint.java
@@ -2,66 +2,62 @@ package br.com.devdojo.endpoint;
 
 import br.com.devdojo.error.CustomErrorType;
 import br.com.devdojo.model.Student;
-import br.com.devdojo.util.DateUtil;
+import br.com.devdojo.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-import static java.util.Arrays.asList;
-
 @RestController
 @RequestMapping("students")
 public class StudentEndpoint {
-    private final DateUtil dateUtil;
+    private final StudentRepository studentDAO;
 
     @Autowired
-    public StudentEndpoint(DateUtil dateUtil) {
-        this.dateUtil = dateUtil;
+    public StudentEndpoint(StudentRepository studentDAO) {
+        this.studentDAO = studentDAO;
     }
 
 //    @RequestMapping(method = RequestMethod.GET)
     @GetMapping // mesma coisa que o de cima
     public ResponseEntity<?> listAll() {
-        return new ResponseEntity<>(Student.students, HttpStatus.OK);
+        return new ResponseEntity<>(studentDAO.findAll(), HttpStatus.OK);
     }
 
 //    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
     @GetMapping(path = "/{id}") // mesma coisa que o de cima
-    public ResponseEntity<?> getStudentId(@PathVariable("id") int id) {
-        // --- temporário
-        Student student = new Student();
-        student.setId(id);
-        int index = Student.students.indexOf(student);
-        // --- temporário
-
-        if (index == -1)
+    public ResponseEntity<?> getStudentId(@PathVariable("id") Long id) {
+        if (studentDAO.findById(id).isPresent()) {
+            Student student = studentDAO.findById(id).get();
+            return new ResponseEntity<>(student, HttpStatus.OK);
+        } else {
             return new ResponseEntity<>(new CustomErrorType("Student not found"), HttpStatus.NOT_FOUND);
-        else
-            return new ResponseEntity<>(Student.students.get(index), HttpStatus.OK);
+        }
+    }
+
+    @GetMapping(path = "/findByName/{name}")
+    public ResponseEntity<?> findStudentsByName(@PathVariable String name) {
+        return new ResponseEntity<>(studentDAO.findByNameIgnoreCaseContaining(name), HttpStatus.OK);
     }
 
 //    @RequestMapping(method = RequestMethod.POST)
     @PostMapping // mesma coisa que o de cima
     public ResponseEntity<?> save(@RequestBody Student student) {
-        Student.students.add(student);
-        return new ResponseEntity<>(student, HttpStatus.OK);
+        return new ResponseEntity<>(studentDAO.save(student), HttpStatus.CREATED);
     }
 
 //    @RequestMapping(method = RequestMethod.DELETE)
-    @DeleteMapping // mesma coisa que o de cima
-    public ResponseEntity<?> delete(@RequestBody Student student) {
-        Student.students.remove(student);
+//    @DeleteMapping // mesma coisa que o de cima
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        studentDAO.deleteById(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
 //    @RequestMapping(method = RequestMethod.PUT)
     @PutMapping // mesma coisa que o de cima
     public ResponseEntity<?> update(@RequestBody Student student) {
-        int index = Student.students.indexOf(student);
-        Student.students.set(index, student);
+        studentDAO.save(student);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/br/com/devdojo/model/Student.java
+++ src/main/java/br/com/devdojo/model/Student.java
@@ -1,46 +1,10 @@
 package br.com.devdojo.model;
 
-import org.springframework.stereotype.Component;
+import javax.persistence.Entity;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-
-import static java.util.Arrays.asList;
-
-@Component
-public class Student {
-    private int id;
+@Entity
+public class Student extends AbstractEntity {
     private String name;
-    public static List<Student> students;
-
-    static {
-        studentRepository();
-    }
-
-    public Student(int id, String name) {
-        this(name);
-        this.id = id;
-    }
-
-    public Student(String name) {
-        this.name = name;
-    }
-
-    public Student() {
-    }
-
-    private static void studentRepository() {
-        students = new ArrayList<>(asList(new Student(1, "Bedrich"), new Student(2, "Godfrey")));
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
@@ -48,18 +12,5 @@ public class Student {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Student student = (Student) o;
-        return id == student.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
     }
 }
