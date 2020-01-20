cb015d9b8ccba1656d1214c821b89f0e07ad4876
Arthur Vila Nova <arthurjvnb@yahoo.com.br>
2020-01-20 20:58:47 -0300

    implementacoes ate aula 28 -incompleto (obs: testes nao estao conseguindo fazer a autenticacao, o que leva a varias falhas)

--- src/test/java/br/com/devdojo/StudentEndpointTest.java
+++ src/test/java/br/com/devdojo/StudentEndpointTest.java
@@ -2,6 +2,8 @@ package br.com.devdojo;
 
 import br.com.devdojo.model.Student;
 import br.com.devdojo.repository.StudentRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +24,7 @@ import java.util.Optional;
 
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpMethod.DELETE;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc // Deve ser colocado caso usarmos o MockMvc
@@ -44,6 +47,12 @@ public class StudentEndpointTest {
         }
     }
 
+    @BeforeEach
+    void init() {
+        Student student = new Student(1L, "Aragorn", "reidegondor@lotr.com");
+        BDDMockito.when(studentRepository.findById(student.getId())).thenReturn(Optional.of(student));
+    }
+
     @Test
     public void listStudentsWhenUsernameAndPasswordAreIncorrectShouldReturnStatusCode401() {
         restTemplate = restTemplate.withBasicAuth("1", "1");
@@ -53,45 +62,58 @@ public class StudentEndpointTest {
         */
 
         ResponseEntity<String> responseEntity = restTemplate.getForEntity("/v1/protected/students/", String.class);
-        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(401);
+//        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(401);
+        Assertions.assertEquals(401, responseEntity.getStatusCodeValue()); // Forma nova do pacote org.junit.jupiter.api.Assertions
     }
 
     @Test
     public void getStudentByIdWhenUsernameAndPasswordAreIncorrectShouldReturnStatusCode401() {
         restTemplate = restTemplate.withBasicAuth("1", "1");
         ResponseEntity<String> responseEntity = restTemplate.getForEntity("/v1/protected/students/1", String.class);
-        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(401);
+//        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(401);
+        Assertions.assertEquals(401, responseEntity.getStatusCodeValue()); // Forma nova do pacote org.junit.jupiter.api.Assertions
     }
 
     @Test
     public void listStudentsWhenUsernameAndPasswordAreCorrectShouldReturnStatusCode200() {
+        System.out.println(restTemplate);
         List<Student> students = asList(
                 new Student(1L, "Aragorn", "reidegondor@lotr.com"),
-                new Student(2L, "Legolas", "folha@lotr.com")
+                new Student(2L, "Legolas", "folhaverde@lotr.com")
         );
 
         // Aqui simula qual deveria ser o retorno para o método que definimos nesse Mock abaixo
         BDDMockito.when(studentRepository.findAll()).thenReturn(students);
 
         // TODO: Descobrir porque esse teste não está conseguindo autenticar o usuário corretamente
-        ResponseEntity<String> responseEntity = restTemplate.getForEntity("/v1/protected/students/", String.class);
-        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(200);
+        ResponseEntity<String> responseEntity = restTemplate.withBasicAuth("athena", "deusa").getForEntity("/v1/protected/students/", String.class);
+        System.out.println(responseEntity);
+//        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(200);
+        Assertions.assertEquals(200, responseEntity.getStatusCodeValue()); // Forma nova do pacote org.junit.jupiter.api.Assertions
     }
 
     @Test
     public void getStudentByIdWhenUsernameAndPasswordAreCorrectShouldReturnStatusCode200() {
-        Student student = new Student(1L, "Gmili", "gmili@lotr.com");
-        BDDMockito.when(studentRepository.findById(student.getId())).thenReturn(Optional.of(student));
         // TODO: Descobrir porque esse teste não está conseguindo autenticar o usuário corretamente
-        ResponseEntity<String> responseEntity = restTemplate.getForEntity("/v1/protected/students/{id}", String.class, student.getId());
-        //ResponseEntity<String> responseEntity = restTemplate.getForEntity("/v1/protected/students/" + student.getId(), String.class); // Tbm pode ser assim.
-        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(200);
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity("/v1/protected/students/{id}", String.class, 1L);
+        //ResponseEntity<String> responseEntity = restTemplate.getForEntity("/v1/protected/students/" + 1L, String.class); // Tbm pode ser assim.
+//        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(200);
+        Assertions.assertEquals(200, responseEntity.getStatusCodeValue()); // Forma nova do pacote org.junit.jupiter.api.Assertions
     }
 
     @Test
     public void getStudentByIdWhenUsernameAndPasswordAreCorrectAndStudentDoesNotExistShouldReturnStatusCode404() {
         // TODO: Descobrir porque esse teste não está conseguindo autenticar o usuário corretamente
         ResponseEntity<String> responseEntity = restTemplate.getForEntity("/v1/protected/students/{id}", String.class, -1L);
-        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(404);
+//        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(404);
+        Assertions.assertEquals(404, responseEntity.getStatusCodeValue()); // Forma nova do pacote org.junit.jupiter.api.Assertions
+    }
+
+    @Test
+    public void deleteWhenUserHasRoleAdminAndStudentExistsShouldReturnStatusCode200() {
+        BDDMockito.doNothing().when(studentRepository).deleteById(1L);
+        ResponseEntity<String> exchange = restTemplate.withBasicAuth("athena", "deusa").exchange("/v1/admin/delete/{id}", DELETE, null, String.class, 1L);
+//        assertThat(exchange.getStatusCodeValue()).isEqualTo(200);
+        Assertions.assertEquals(200, exchange.getStatusCodeValue()); // Forma nova do pacote org.junit.jupiter.api.Assertions
     }
 }
---
+++ src/test/java/br/com/devdojo/StudentEndpointTest_v2.java
@@ -0,0 +1,38 @@
+package br.com.devdojo;
+
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class StudentEndpointTest_v2 {
+    @Autowired
+    private WebApplicationContext context;
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    public void listStudentsWhenUsernameAndPasswordAreIncorrectShouldReturnStatusCode401() throws Exception {
+        mockMvc.perform(get("/v1/protected/students").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+    }
+}
